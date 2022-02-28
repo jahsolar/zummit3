063771663b4f529f663c58e161103ed03fd2de7c
Abdalla Hisham <abdallaprogrammer@gmail.com>
2022-02-28 18:49:53 +0200

    Use composer dependencies

--- mattix.php
+++ mattix.php
@@ -1,16 +1,9 @@
 <?php
-	require_once ("Classes/PHPExcel.php");
-	$path = "mattix.xlsx";
-	$reader = PHPExcel_IOFactory::createReaderForFile($path);
-	$excel_obj = $excelReader->load($path);
-	// $worksheet = $excel_obj->getActiveSheet();
-	$worksheet = $excel_obj->getSheet('0');
+require __DIR__ . '/vendor/autoload.php';
 
-	echo $worksheet->getCell('A1')->getValue();
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
-	// require __DIR__ . '/vendor/autoload.php';
-	// $reader = new \PhpSpreadsheet\src\PhpSpreadsheet\Reader\Xlsx();
-	// $spreadsheet = $reader->load("mattix.xlsx");
-	// $worksheet = $spreadsheet->getSheet('0');
-	// echo $worksheet->getCell('A1')->getValue();
-?>
+$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+$spreadsheet = $reader->load("mattix.xlsx");
+$worksheet = $spreadsheet->getSheet('0');
+echo $worksheet->getCell('A1')->getValue();
